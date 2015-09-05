32a3cc4f6269fe9784ae5271c01db70a2d7582cb
Daniel Fiala <freelancer@meave.biz>
2015-09-05 14:29:46 +0200

    Added files from R Programming Course, programming assigment 2.

---
+++ Rprog/ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,30 @@
+## These functions implement caching of calculation of inverse matrix
+##
+
+## This function makes object with set. get, setinverse and getinverse methods.
+
+makeCacheMatrix <- function(x = matrix()) {
+	x.inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		x.inverse <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inv) x.inverse <<- inv
+	getinverse <- function() x.inverse
+	list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+
+## Returns inverse matrix to matrix x. Matrix must be created with
+##   makeCacheMatrix. If inverse matrix is calculated already and is
+##   cached in the object, then it is not calculated again.
+
+cacheSolve <- function(x, ...) {
+	## Return a matrix that is the inverse of 'x'
+	inv <- x$getinverse()
+	if (is.null(inv)) {
+		x$setinverse(inv <- solve(x$get(), ...))
+	}
+	inv
+}
