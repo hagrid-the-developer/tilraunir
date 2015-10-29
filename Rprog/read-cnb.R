a96fc3194e1c997ab1c808a95d5b1a9f605ce93a
Daniel Fiala <freelancer@meave.biz>
2015-10-29 22:05:40 +0100

    R script for reading of the Czech National Bank exchange rates.

---
+++ Rprog/read-cnb.R
@@ -0,0 +1,17 @@
+#' Returns currency exchange rates according to the Czech National Bank
+#'
+#' @param date Date in the form 'DD.MM.YYYY'
+#'
+#' @return Currency exchange rates as date.frame .
+
+read.cnb <- function(date=NULL) {
+	url <- 'https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt'
+	if (!is.null(date)) {
+		url <- paste0(url, '?date=', date)
+	}
+	data <- read.csv(url, skip=1, header=TRUE, sep='|', dec=',')
+
+	data
+}
+
+
