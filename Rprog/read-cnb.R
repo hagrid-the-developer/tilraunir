59b7b00c6d4c4f07fddfbf8c5b0173d6b3663743
Daniel Fiala <freelancer@meave.biz>
2015-10-30 07:35:13 +0100

    Rfunc read.cnb(): typo and simplification.

--- Rprog/read-cnb.R
+++ Rprog/read-cnb.R
@@ -2,16 +2,14 @@
 #'
 #' @param date Date in the form 'DD.MM.YYYY'
 #'
-#' @return Currency exchange rates as date.frame .
+#' @return Currency exchange rates as data.frame .
 
 read.cnb <- function(date=NULL) {
 	url <- 'https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt'
 	if (!is.null(date)) {
 		url <- paste0(url, '?date=', date)
 	}
-	data <- read.csv(url, skip=1, header=TRUE, sep='|', dec=',')
-
-	data
+	read.csv(url, skip=1, header=TRUE, sep='|', dec=',')
 }
 
 
