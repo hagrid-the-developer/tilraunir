bc67e9f9f71df430e66ab47dbc41a53172242a63
Daniel Fiala <freelancer@meave.biz>
2015-12-26 11:35:05 +0100

    Added plot6.

--- Rprog/DataScience/ExploratoryDataAnalysis/ProgrammingAssignment2/plot6.R
+++ Rprog/DataScience/ExploratoryDataAnalysis/ProgrammingAssignment2/plot6.R
@@ -15,11 +15,18 @@ plot.6 <- function() {
 	# We want only items for Baltimore city and Los Angeles County.
 	NEI <- NEI[NEI$fips %in% c('24510', '06037'),]
 
+
 	# Add name of the city
 	NEI$City <- ifelse(NEI$fips == '24510', 'Baltimore City', 'Los Angeles County')
 
 	dt <- aggregate(Emissions ~ year + City, data=NEI, FUN=sum)
-	ggplot(dt, aes(x=factor(year), y=Emissions, group=1)) + geom_point(stat = 'identity', size = 2) + geom_line() + facet_grid(. ~ City)
+	dt[dt$City == 'Baltimore City', ]$Emissions <- dt[dt$City == 'Baltimore City', ]$Emissions - min(dt[dt$City == 'Baltimore City', ]$Emissions)
+	dt[dt$City == 'Los Angeles County', ]$Emissions <- dt[dt$City == 'Los Angeles County', ]$Emissions - min(dt[dt$City == 'Los Angeles County', ]$Emissions)
+	ggplot(dt, aes(x=year, y=Emissions, group=1)) + geom_point(stat = 'identity', size = 2) + geom_line() + facet_grid(. ~ City) +
+		ggtitle('Emissions from motor vehicle sources\nminus minimal value over years\n(in Baltimore City and in Los Angeles County)') +
+		scale_x_continuous(name='Year') +
+		scale_y_continuous(name='Emissions (tons) - min')
+	ggsave('plot6.png', width=10, height=10)
 }
 
-#plot.6()
+plot.6()
