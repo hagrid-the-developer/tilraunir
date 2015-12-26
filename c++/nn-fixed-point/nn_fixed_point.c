4b8bd8ab45436b65320d5de248d1a5165d09bcbc
Daniel Fiala <freelancer@meave.biz>
2015-12-26 20:47:55 +0100

    Fixed point can compile. :-)

--- c++/nn-fixed-point/nn_fixed_point.c
+++ c++/nn-fixed-point/nn_fixed_point.c
@@ -1,5 +1,5 @@
 #include <immintrin.h>
-#include <assert>
+#include <assert.h>
 
 #include "nn_fixed_point.h"
 
@@ -9,13 +9,13 @@ void cache_block_over_inputs(const u8 *w, const u8 *inputs, const u8 *outputs, c
 	assert(cache_blocking_len > 0);
 
 
-	__mm256i part_results[cache_blocking_len];
+	__m256i part_results[cache_blocking_len];
 
 	for (uns index = 0; index < w_len; index += cache_blocking_len) {
 		const uns jndex_end = MIN(w_len, index + cache_blocking_len);
 
 		for (uns cb_index = 0; cb_index < cache_blocking_len; ++cb_index) {
-			for (uns jndex = index; jindex < jindex_end; jndex += AVX_U8_VEC_LEN) {
+			for (uns jndex = index; jndex < jndex_end; jndex += AVX_U8_VEC_LEN) {
 				const __m256i *weight = (__m256i*) &w[jndex + cb_index*w_len];
 				const __m256i *input = (__m256i*) &input[jndex];
 				const __m256i sum = _mm256_maddubs_epi16(*weight, *input);
@@ -28,7 +28,7 @@ void cache_block_over_inputs(const u8 *w, const u8 *inputs, const u8 *outputs, c
 
 	for (uns cb_index = 0; cb_index < cache_blocking_len; cb_index += cache_blocking_len) {
 		// _mm256_permute2x128_si256: http://www.felixcloutier.com/x86/VPERM2I128.html
-		// __m256i _mm256_shuffle_epi8: https://software.intel.com/en-us/node/582929
+		// _mm256_shuffle_epi8: https://software.intel.com/en-us/node/582929
 		// _mm256_hadds_epi16: https://software.intel.com/en-us/node/582799, http://www.felixcloutier.com/x86/PHADDSW.html
 		// _mm256_blendv_epi8: https://software.intel.com/en-us/node/582820
 		// _mm256_shuffle_epi8: https://software.intel.com/en-us/node/582929
@@ -36,51 +36,52 @@ void cache_block_over_inputs(const u8 *w, const u8 *inputs, const u8 *outputs, c
 		// _mm256_srai_epi16: https://software.intel.com/en-us/node/582815
 		// _mm256_setr_epi64x: https://software.intel.com/sites/landingpage/IntrinsicsGuide/#text=_mm256_setr_epi64x&expand=4649
 
-
 		// Create 2 128bit parts with 16bit integers.
-#define SUM(X, Y) \
+#define SUM_2x128(X, Y) \
 	const __m256i x##X = part_results[cb_index + X]; \
 	const __m256i x##Y = part_results[cb_index + Y]; \
 	__m256i sum##X = _mm256_adds_epi16( _mm256_permute2x128_si256(x##X, x##Y, 0x20), _mm256_permute2x128_si256(x##X, x##Y, 0x31) )	
 
-		SUM(0, 1);
-		SUM(2, 3);
-		SUM(4, 5);
-		SUM(6, 7);
-		SUM(8, 9);
-		SUM(10, 11);
-		SUM(12, 13);
-		SUM(14, 15);
+		SUM_2x128(0, 1);
+		SUM_2x128(2, 3);
+		SUM_2x128(4, 5);
+		SUM_2x128(6, 7);
+		SUM_2x128(8, 9);
+		SUM_2x128(10, 11);
+		SUM_2x128(12, 13);
+		SUM_2x128(14, 15);
 
-#undef SUM
+#undef SUM_2x128
 
 		// Create 4 64bit parts with 16bit integers.
-#define SUM(X, Y) \
-	sum##X = _m256_adds_epi(_mm256_permute2x128_si256(_mm256_permute4x64_epi64(sum##X, 0x20), _mm256_permute4x64_epi64(sum##Y, 0x20), 0x20), \
+#define SUM_4x64(X, Y) \
+	sum##X = _mm256_adds_epi16(_mm256_permute2x128_si256(_mm256_permute4x64_epi64(sum##X, 0x20), _mm256_permute4x64_epi64(sum##Y, 0x20), 0x20), \
 				_mm256_permute2x128_si256(_mm256_permute4x64_epi64(sum##X, 0x31), _mm256_permute4x64_epi64(sum##Y, 0x31), 0x20))
 
-		SUM(0, 2);
-		SUM(4, 6);
-		SUM(8, 10);
-		SUM(12, 14);
-#undef SUM
+		SUM_4x64(0, 2);
+		SUM_4x64(4, 6);
+		SUM_4x64(8, 10);
+		SUM_4x64(12, 14);
+#undef SUM_4x64
 
 		// Create 8 32bit parts with 16bit integers.
-#define SUM(X, Y) \
-	sum##X = _m256_adds_epi(_mm256_permute2x128_si256(_mm256_permutevar8x32_epi32(x##X, 0x6420), _mm256_permutevar8x32_epi32(x##Y, 0x6420), 0x20), \
-				_mm256_permute2x128_si256(_mm256_permutevar8x32_epi32(x##X, 0x7531), _mm256_permutevar8x32_epi32(x##Y, 0x7531), 0x20))
+#define SUM_8x32(X, Y) \
+	sum##X = _mm256_adds_epi16(_mm256_permute2x128_si256(_mm256_permutevar8x32_epi32(x##X, _mm256_setr_epi32(0, 0, 0, 0, 6, 4, 2, 0)), \
+							     _mm256_permutevar8x32_epi32(x##Y, _mm256_setr_epi32(0, 0, 0, 0, 6, 4, 2, 0)), 0x20), \
+				   _mm256_permute2x128_si256(_mm256_permutevar8x32_epi32(x##X, _mm256_setr_epi32(0, 0, 0, 0, 7, 5, 3, 1)), \
+					   		     _mm256_permutevar8x32_epi32(x##Y, _mm256_setr_epi32(0, 0, 0, 0, 7, 5, 3, 1)), 0x20))
 
-		SUM(0, 4);
-		SUM(8, 12);
-#undef SUM
+		SUM_8x32(0, 4);
+		SUM_8x32(8, 12);
+#undef SUM_8x32
 
 		// Create 16 parts with 16bit integers.
 		sum0 = _mm256_hadds_epi16(sum0, sum8);
 
 		// Final operations.
 		sum0 = _mm256_max_epi16(sum0, _mm256_setzero_si256());
-		sum0 = _m256_srai_epi16(sum0, 8);
-		sum0 = _mm256_shuffle_epi8(sum0 << 8, sum0, _mm256_setr_epi64x());
+		sum0 = _mm256_srai_epi16(sum0, 8);
+
 		// FIXME: Add last conversion of 16bit integers to 8bit integers.
 		// stream store, type conversions seem ugly...
 		_mm_stream_ps((float*)&outputs[cb_index], (__m128)_mm256_castsi256_si128(sum0));
