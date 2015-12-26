488fb41cb907489ceee2e06b69ff3001caa9fd4f
Daniel Fiala <freelancer@meave.biz>
2015-12-26 13:27:26 +0100

    Added Makefile and c++ header file.

--- c++/nn-fixed-point/nn_fixed_point.h
+++ c++/nn-fixed-point/nn_fixed_point.h
@@ -1,5 +1,13 @@
+#ifndef NN_FIXED_POINT_H_INCLUDED
+#define NN_FIXED_POINT_H_INCLUDED
+
 #define AVX_U8_VEC_LEN 32
 #define MIN(X, Y) ((X) <= (Y) ? (X) : (Y))
 
 typedef unsigned char u8;
 typedef unsigned uns;
+
+void cache_block_over_inputs(const u8 *w, const u8 *inputs, const u8 *outputs, const uns w_len, const uns cache_blocking_len);
+void cache_block_over_inputs_floats(const u8 *weights, const u8 *inputs, const uns w_len, const uns cache_blocking_len);
+
+#endif /* NN_FIXED_POINT_H_INCLUDED */
---
+++ c++/nn-fixed-point/nn_fixed_point.hpp
@@ -0,0 +1,8 @@
+#ifndef NN_FIXED_POINT_HPP_INCLUDED
+#define NN_FIXED_POINT_HPP_INCLUDED
+
+extern "C" {
+#include "nn_fixed_point.h"
+}
+
+#endif /* NN_FIXED_POINT_HPP_INCLUDED */
