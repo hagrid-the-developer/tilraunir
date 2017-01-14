1a6859308ebf41aaf262e5f638fad4b9783f1fae
Daniel Fiala <freelancer@meave.biz>
2017-01-14 19:23:54 +0100

    Experiments with union initializations.

--- c++/union/union.cpp
+++ c++/union/union.cpp
@@ -9,10 +9,21 @@
 namespace {
 
 union X {
+	// Trick with { x_ = } works only if no contructor is defined!
 	void *v_;
 	unsigned char *u8_;
 	float *f_;
+
+	explicit operator bool() {
+		return v_ != nullptr;
+	}
 };
+
+float pi = 3.14;
+
+X ret_x() {
+	return X{.f_ = &pi};
+}
 
 } /* Anonymous Namespace */
 
