3ec4749aa542a44b63d67cf4cc0dbbad6de6a1c5
Daniel Fiala <freelancer@meave.biz>
2017-02-01 14:47:58 +0100

    Added test for AVX doublexor.

--- c++/horizontal-doublexor/horizontal-doublexor.cpp
+++ c++/horizontal-doublexor/horizontal-doublexor.cpp
@@ -47,10 +47,10 @@ T init() {
 
 template <typename T>
 ::uint64_t naive(const T x, const T y) {
-	::uint32_t xor_x = 0;
-	::uint32_t xor_y = 0;
+	::uint32_t xor_x = x.dw_[0];
+	::uint32_t xor_y = y.dw_[0];
 
-	for (::size_t i = 0; i < sizeof(T) / 4; ++i) {
+	for (::size_t i = 1; i < sizeof(T) / 4; ++i) {
 		xor_x ^= x.dw_[i];
 		xor_y ^= y.dw_[i];
 	}
@@ -67,6 +67,19 @@ template <typename T>
 		res1.f4_ = _mm_movehl_ps(res1.f4_, SSE{.i4_ = _mm_slli_epi64(res0.i4_, 32)}.f4_);
 		res0.i4_ = _mm_or_si128(res0.i4_, res1.i4_);
 		return _mm_cvtsi128_si64(res0.i4_);
+}
+
+::uint64_t avx(AVX x0, AVX x1) {
+	x0.sse_[0].i4_ = _mm_xor_si128(x0.sse_[0].i4_, SSE{.f4_ = _mm256_extractf128_ps(x0.f8_, 1)}.i4_);
+	x1.sse_[0].i4_ = _mm_xor_si128(x1.sse_[0].i4_, SSE{.f4_ = _mm256_extractf128_ps(x1.f8_, 1)}.i4_);
+	SSE res0{ .d2_ = _mm_unpacklo_pd(x0.sse_[0].d2_, x1.sse_[0].d2_) };
+	SSE res1{ .d2_ = _mm_unpackhi_pd(x0.sse_[0].d2_, x1.sse_[0].d2_) };
+	res0.f4_ = _mm_xor_ps(res0.f4_, res1.f4_);
+	res1.f4_ = _mm_movehdup_ps(res0.f4_);
+	res0.f4_ = _mm_xor_ps(res0.f4_, res1.f4_);
+	res1.f4_ = _mm_movehl_ps(res1.f4_, SSE{.i4_ = _mm_slli_epi64(res0.i4_, 32)}.f4_);
+	res0.i4_ = _mm_or_si128(res0.i4_, res1.i4_);
+	return _mm_cvtsi128_si64(res0.i4_);
 }
 
 template <typename T>
@@ -89,7 +102,18 @@ bool run_sse() {
 	const auto val0 = naive(x0, x1);
 	const auto val1 = sse(x0, x1);
 	
-	fprintf(stderr, "x0:%s x1:%s val0:0x%.16jx val1:0x%.16jx\n", $$($(x0)), $$($(x1)), ::uintmax_t(val0), ::uintmax_t(val1));
+	fprintf(stderr, "SSE: x0:%s x1:%s val0:0x%.16jx val1:0x%.16jx\n", $$($(x0)), $$($(x1)), ::uintmax_t(val0), ::uintmax_t(val1));
+
+	return val0 == val1;
+}
+
+bool run_avx() {
+	const AVX x0 = init<AVX>();
+	const AVX x1 = init<AVX>();
+	const auto val0 = naive(x0, x1);
+	const auto val1 = avx(x0, x1);
+
+	fprintf(stderr, "AVX: x0:%s x1:%s val0:0x%.16jx val1:0x%.16jx\n", $$($(x0)), $$($(x1)), ::uintmax_t(val0), ::uintmax_t(val1));
 
 	return val0 == val1;
 }
@@ -101,6 +125,8 @@ main(void) {
 	for (int _ = 0; _ < 256; ++_) {
 		if (!run_sse())
 			return 1;
+		if (!run_avx())
+			return 2;
 	}
 
 	return 0;
