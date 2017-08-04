3c080cf155a86a0c4953a6e5b7a5123508b11da4
Daniel Fiala <freelancer@meave.biz>
2017-08-04 18:14:30 +0200

    Experiments with gatherd (backup commit).

--- asm/gather/gather.h
+++ asm/gather/gather.h
@@ -5,6 +5,7 @@
 
 #include <cstdint>
 #include <immintrin.h>
+#include <initializer_list>
 
 union AVX {
 	__m256i i8_;
@@ -17,9 +18,22 @@ union AVX {
 	::uint32_t dw_[8];
 	::uint64_t qw_[4];
 	::uint8_t b_[32];
+
+	AVX() : f8_(_mm256_setzero_ps()) {}
+	AVX(const __m256i $) : i8_($) {}
+	AVX(const std::initializer_list<::uint32_t> &$) {
+		unsigned i = 0;
+		for (auto _: $) {
+			dw_[i++] = _;
+		}
+		while (i < 8) {
+			dw_[i++] = 0;
+		}
+	}
 };
 
 void run_movps(const float *src, const uint64_t len_div_32);
 void run_gatherd(const float *src, const uint64_t len_div_32, const __m256i steps);
+__m256i run_gatherd_sum(const float *src, const uint64_t len_div_32, const __m256i steps);
 
 #endif
--- asm/gather/test.cpp
+++ asm/gather/test.cpp
@@ -13,6 +13,22 @@ namespace {
 
 const ::size_t MAX_LEN = 128*1024*1024;
 
+AVX step_sum(const float *p, const ::size_t len, const AVX steps) {
+	AVX sum;
+	for (::size_t i = 0; i < len; ++i) {
+		sum.f8_[0] += p[i + 0*steps.i8_[0]];
+		sum.f8_[1] += p[i + 1*steps.i8_[1]];
+		sum.f8_[2] += p[i + 2*steps.i8_[2]];
+		sum.f8_[3] += p[i + 3*steps.i8_[3]];
+		sum.f8_[4] += p[i + 4*steps.i8_[4]];
+		sum.f8_[5] += p[i + 5*steps.i8_[5]];
+		sum.f8_[6] += p[i + 6*steps.i8_[6]];
+		sum.f8_[7] += p[i + 7*steps.i8_[7]];
+	}
+	return sum;
+}
+
+
 template <::size_t LEN = MAX_LEN, ::size_t STEP = MAX_LEN>
 void test_step() {
 	static_assert(LEN <= MAX_LEN, "Exceeds maximum len :-(");
@@ -46,13 +62,27 @@ void test_step() {
 	const auto end_mov = gettime();
 	fprintf(stderr, "\tmovs: %lf\n", end_mov - beg_mov);
 
-	AVX steps{ .dw_ = {0*::uint32_t(step), ::uint32_t(step), 2*::uint32_t(step), 3*::uint32_t(step), 4*::uint32_t(step), 5*::uint32_t(step), 6*::uint32_t(step), 7*::uint32_t(step)} };
+	AVX steps{ 0*::uint32_t(step), ::uint32_t(step), 2*::uint32_t(step), 3*::uint32_t(step), 4*::uint32_t(step), 5*::uint32_t(step), 6*::uint32_t(step), 7*::uint32_t(step) };
 
 	const auto beg_gatherd = gettime();
 	for (::size_t i = repeat; i--; )
-		run_gatherd(p, len_div_32, steps.i8_);
+		::run_gatherd(p, len_div_32, steps.i8_);
 	const auto end_gatherd = gettime();
 	fprintf(stderr, "\tgatherd: %lf\n", end_gatherd - beg_gatherd);
+
+	// Testing that gatherd works correctly.
+	for (::size_t i = repeat; i--; ) {
+		const AVX sum_real{ ::run_gatherd_sum(p, len_div_32, steps.i8_) };
+		const AVX sum_expected = step_sum(p, len_div_32, steps);
+		for (unsigned i = 0; i < 8; ++i) {
+			if (sum_real.f8_[i] != sum_expected.f8_[i]) {
+				fprintf(stderr, "repeat:%zu, real and expected sums differs: (%f, %f, %f, %f, %f, %f, %f, %f) != (%f, %f, %f, %f, %f, %f, %f, %f)\n", repeat,
+						sum_real.f8_[0], sum_real.f8_[1], sum_real.f8_[2], sum_real.f8_[3], sum_real.f8_[4], sum_real.f8_[5], sum_real.f8_[6], sum_real.f8_[7],
+						sum_expected.f8_[0], sum_expected.f8_[1], sum_expected.f8_[2], sum_expected.f8_[3], sum_expected.f8_[4], sum_expected.f8_[5], sum_expected.f8_[6], sum_expected.f8_[7]);
+				::abort();
+			}
+		}
+	}
 
 	::free(p);
 }
