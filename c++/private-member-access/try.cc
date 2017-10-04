dc81a905e8c6bfaf07deebac06a8d1553b048cfb
Daniel Fiala <freelancer@meave.biz>
2017-10-04 05:07:34 +0200

    Fiexd comment.

--- c++/private-member-access/try.cc
+++ c++/private-member-access/try.cc
@@ -1,5 +1,5 @@
 /*
- * Test whether implicit instantion of the template allows accwessing
+ * Test whether explicit instantion of the template allows accwessing
  *   of private members of the class:
  *   * https://github.com/insooth/insooth.github.io/blob/master/accessing-private-members.md
  *  It seems to work (tested fopr gcc 4.92 - 7.1.0 and clang 3.8).
