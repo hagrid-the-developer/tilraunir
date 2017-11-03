92fb478b1d3dc30d09543097c751a2c6e96b6d11
Daniel Fiala <freelancer@meave.biz>
2017-11-03 07:35:14 +0100

    Other experiemnts in python.

--- python/check-vat/vat.py
+++ python/check-vat/vat.py
@@ -4,6 +4,8 @@ import sys
 import zeep
 from zeep import Client
 
+import json
+
 
 def help():
     print 'Usage:'
@@ -19,18 +21,35 @@ def check_vat(vat_id):
     ret = client.service.checkVat(prefix, number)
     return ret['valid']
 
-if len(sys.argv) != 2:
-    help()
+def parse_json(s):
+    decoder = json.JSONDecoder()
+    d = decoder.decode(s)
+    if 'vat-id' in d:
+        return d['vat-id']
+    else:
+        raise ValueError('vat-id item is missing')
+
 
 if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-    help
+    help()
 
 try:
-    if check_vat(sys.argv[1]):
+    if sys.argv[1] == '--json':
+        valid = check_vat(parse_json(sys.argv[2]))
+    elif len(sys.argv) == 2:
+        valid = check_vat(sys.argv[1])
+    else:
+        help()
+    
+    if valid:
         print 'Valid'
     else:
         print 'Invalid'
+
 except zeep.exceptions.Fault as e:
     print 'Cannot parse VAT-Number: %s' % e
+except ValueError as e:
+    print 'Cannot parse json: %s' %e
 except:
     print 'Cannot determine whether VAT-number is valid: %s' % sys.exc_info()[1]
+
