9c36068e49b248da0b0d6da0b728bc4e673116f5
Daniel Fiala <freelancer@meave.biz>
2017-11-12 12:00:33 +0100

    Added another case to the test.

--- java/big_decimal_rounding/test-big-decimal.java
+++ java/big_decimal_rounding/test-big-decimal.java
@@ -9,6 +9,7 @@ class TestBigDecimal {
 		System.out.println(new BigDecimal(new BigInteger("1"), -10).divide(new BigDecimal(new BigInteger("3"), -3), BigDecimal.ROUND_HALF_EVEN).toString());
 		// This returns 0E10, but I think it should return 3E10.
 		System.out.println(new BigDecimal(new BigInteger("10"), -10).divide(new BigDecimal(new BigInteger("3"), -3), BigDecimal.ROUND_HALF_EVEN).toString());
+		System.out.println(new BigDecimal("10E10").divide(new BigDecimal("3"), BigDecimal.ROUND_HALF_EVEN).toString());
 		System.out.println(new BigDecimal("10").divide(new BigDecimal("3"), BigDecimal.ROUND_HALF_EVEN).toString());
 		System.out.println(new BigDecimal("10.000").divide(new BigDecimal("3"), BigDecimal.ROUND_HALF_EVEN).toString());
 		System.out.println(new BigDecimal("15").divide(new BigDecimal("10"), BigDecimal.ROUND_HALF_EVEN).toString());
