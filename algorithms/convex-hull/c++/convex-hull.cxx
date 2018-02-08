0c40a962ba5ee8dbcd336d51e4b64d59b5f49518
Daniel Fiala <freelancer@meave.biz>
2018-02-08 20:54:42 +0100

    Simple algorithm for convex-hull in C++.

--- algorithms/convex-hull/c++/convex-hull.cxx
+++ algorithms/convex-hull/c++/convex-hull.cxx
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <tuple>
 #include <type_traits>
@@ -29,23 +30,83 @@ static T cross_product(const Point<T>& a, const Point<T>& b, const Point<T>& c) 
 	return (a.x() - c.x())*(b.y() - c.y()) - (a.y() - c.y())*(b.x() - c.x());
 }
 
-template <typename T>
-struct PointInContainer { };
 
 template <typename T>
-struct PointInContainer<Point<T>> {
-	using type = Point<T>;
+struct reversion_wrapper {
+	T& _iterable;
 };
+
+template <typename T>
+reversion_wrapper<T> reverse(T&& iterable) {
+	return { iterable };
+}
+
+template <typename T>
+struct PointType { };
+
+template <typename T>
+struct PointType<Point<T>> {
+	using type = T;
+};
+
+} /* anonymous namespace */
+
+namespace std {
+
+template <typename T>
+auto begin(reversion_wrapper<T> w) {
+	return rbegin(w._iterable);
+}
+
+template <typename T>
+auto end(reversion_wrapper<T> w) {
+	return rend(w._iterable);
+}
+
+} /* namespace std */
+
+namespace {
 
 template <typename Container>
 static auto convex_hull(Container points) {
 	//using Point = typename std::decay<Container>::type::value_type;
-	using Point = typename PointInContainer<typename std::decay<Container>::type::value_type>::type;
+	using Point = Point< typename PointType<typename std::decay<Container>::type::value_type>::type >;
+
+	auto len = points.size();
+	if (len < 3)
+		return len;
 
 	std::sort(std::begin(points), std::end(points));
 	for (auto point: points) {
 		std::cerr << point << std::endl;
 	}
+
+	std::vector<Point> lower;
+	std::vector<Point> upper;
+
+	for (const auto& x: points) {
+		while ((len = lower.size()) >= 2 && cross_product(lower[len - 2], lower[len - 1], x) <= 0) {
+			lower.pop_back();
+		}
+		lower.emplace_back(x);
+	}
+	std::cerr << std::endl << "Lower: " << std::endl;
+	for (auto point: lower) {
+		std::cerr << point << std::endl;
+	}
+
+	for (const auto& x: reverse(points)) {
+		while ((len = upper.size()) >= 2 && cross_product(upper[len - 2], upper[len - 1], x) <= 0)
+			upper.pop_back();
+		upper.emplace_back(x);
+	}
+	std::cerr << std::endl << "Upper: " << std::endl;
+	for (auto point: upper) {
+		std::cerr << point << std::endl;
+	}
+
+	assert(upper.size() + lower.size() > 4);
+	return upper.size() + lower.size() - 2;
 }
 
 } /* anonymous namespace */
@@ -59,5 +120,5 @@ int main() {
 		, {1, 1}
 		, {4, 4}
 	};
-	convex_hull(std::move(points));
+	std::cout << convex_hull(std::move(points)) << std::endl;
 }
