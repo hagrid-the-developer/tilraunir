ec217827bad847b268bf44a83344046c5b445e9b
Daniel Fiala <freelancer@meave.biz>
2018-05-07 08:42:30 +0200

    Experiments with FFT under python.

---
+++ python/fft/sincossuperposition.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.arange(256)*np.pi / 64.0
+func = np.sin(t) + np.sin(20*t) + np.cos(3*t) + np.cos(17*t)
+
+sp = np.fft.fft(func)
+freq = np.fft.fftfreq(t.shape[-1])
+
+#plt.plot(t, func)
+plt.plot(freq, sp.real, 'b', freq, sp.imag, 'r')
+plt.show()
