1fa0888a7f8af9f96d19f5045380170713fb051f
Daniel Fiala <freelancer@meave.biz>
2018-05-12 22:36:06 +0200

    Added graphs for stochastic neurons and link to Brain2 tutorial.

--- python/brian2/README.md
+++ python/brian2/README.md
@@ -13,13 +13,24 @@ Installation (on Ubuntu 17.10):
 Results:
 --------
 
+Examples taken from [Brian 2 tutorial: Intro to Brian neurons](https://github.com/brian-team/brian2/blob/master/tutorials/1-intro-to-brian-neurons.ipynb).
+
 * multiple-neurons.py:
 
-  * Spikes and firing rate for various indexes:
+  * Spikes and firing rate for various indexes and v0_max values:
 <img src="images/multiple-neurons_firing-rate.png" width=100%>
 
   * Graph of V for neurons with various indexes:
 <img src="images/multiple-neurons_v.png" width=100%>
 
+* stochastic-neuron.py:
+
+  * Spikes and firing rate for various indexes and v0_max values:
+<img src="images/stochastic-neurons_firing-rate.png" width=100%>
+
+  * Graph of V for neurons with various indexes:
+<img src="images/stochastic-neurons_v.png" width=100%>
+
+As one can see, firing rate is non-zero for v0 < 1 and even neuron 0 has some non-zero values for V (but it has no spikes). This is because stochasti deffirential was added to the equations.
 
 
--- python/brian2/stochastic-neuron.py
+++ python/brian2/stochastic-neuron.py
@@ -14,12 +14,13 @@ v0 : 1
 
 G = NeuronGroup(N, eqs, threshold='v>1', reset='v=0', refractory=5*ms, method='euler')
 M = SpikeMonitor(G)
+SM = StateMonitor(G, 'v', record=[0, 25, 50, 75, 99])
 
 G.v0 = 'i*v0_max/(N-1)'
 
 run(duration)
 
-plt.figure(figsize=(12,4))
+plt.figure(figsize=(12,8))
 plt.subplot(121)
 plt.plot(M.t/ms, M.i, '.k')
 plt.xlabel('Time (ms)')
@@ -28,8 +29,29 @@ plt.subplot(122)
 plt.plot(G.v0, M.count/duration)
 plt.xlabel('v0')
 plt.ylabel('Firing rate (sp/s)');
-#subplot(122)
-#plot(G.v99, M.count/duration)
-#xlabel('v99')
-#ylabel('Firing rate (sp/s)');
-plt.show()
+plt.savefig('images/stochastic-neurons_firing-rate.svg', transparent=True)
+plt.savefig('images/stochastic-neurons_firing-rate.png', transparent=True)
+
+plt.figure(figsize=(12,18))
+plt.subplot(511)
+plt.plot(SM.t / ms, SM[99].v / mV)
+plt.xlabel('v[99]')
+plt.ylabel('V[mV]');
+plt.subplot(512)
+plt.plot(SM.t / ms, SM[75].v / mV)
+plt.xlabel('v[75]')
+plt.ylabel('V[mV]');
+plt.subplot(513)
+plt.plot(SM.t / ms, SM[50].v / mV)
+plt.xlabel('v[50]')
+plt.ylabel('V[mV]');
+plt.subplot(514)
+plt.plot(SM.t / ms, SM[25].v / mV)
+plt.xlabel('v[25]')
+plt.ylabel('V[mV]');
+plt.subplot(515)
+plt.plot(SM.t / ms, SM[0].v / mV)
+plt.xlabel('v[0]')
+plt.ylabel('V[mV]');
+plt.savefig('images/stochastic-neurons_v.svg', transparent=True)
+plt.savefig('images/stochastic-neurons_v.png', transparent=True)
