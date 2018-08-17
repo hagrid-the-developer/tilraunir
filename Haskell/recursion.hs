9e5e9c7b8797d41f74f4640bf8d7b9c8dbfadf47
Daniel Fiala <freelancer@meave.biz>
2018-08-17 08:20:47 +0200

    Haskell: Experiments with recursion.

---
+++ Haskell/recursion.hs
@@ -0,0 +1,11 @@
+power x n
+      | n > 1 = x * power x (n - 1)
+      | n == 1 = x
+      | n == 0 = 1
+
+
+log2 n = go 0 1 n
+    where
+    go res pow n
+       | 2*pow > n = res
+       | otherwise = go (res+1) (2*pow) n
