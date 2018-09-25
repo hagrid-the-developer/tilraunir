95db68c44142a694090d36b569c63805e312a852
Daniel Fiala <freelancer@meave.biz>
2018-09-25 07:15:22 +0200

    Implemented edit distance in Rust with zip() instead of enumerate.

--- Rust/edit_distance/src/main.rs
+++ Rust/edit_distance/src/main.rs
@@ -36,6 +36,33 @@ fn edit_distance(s1: &str, s2: &str) -> usize {
     tab[len2 - 1][len1 - 1]
 }
 
+fn edit_distance_zip(s1: &str, s2: &str) -> usize {
+    let len1 = s1.chars().count();
+    let len2 = s2.chars().count();
+
+    // Table with all items isn't necessary
+    let mut tab = vec![vec![0usize; len1]; len2];
+
+    for j in 1..len1 {
+        tab[0][j] = j;
+    }
+    for (c2, i) in s2.chars().skip(1).zip(1usize..) {
+        tab[i][0] = i;
+        for (c1, j) in s1.chars().skip(1).zip(1usize..) {
+            tab[i][j] = if c1 == c2 {
+                tab[i - 1][j - 1]
+            } else {
+                let x = std::cmp::min(tab[i - 1][j], tab[i][j - 1]);
+                let y = tab[i - 1][j - 1];
+                1 + std::cmp::min(x, y)
+            }
+        }
+    }
+
+    tab[len2 - 1][len1 - 1]
+}
+
 fn main() {
     println!("{}", edit_distance("Saturday", "Sunday"));
+    println!("{}", edit_distance_zip("Saturday", "Sunday"));
 }
