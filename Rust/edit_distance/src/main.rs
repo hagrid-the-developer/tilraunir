d09cabbf00af4e97d90997457f42329bdc43e75e
Daniel Fiala <freelancer@meave.biz>
2018-09-25 07:05:51 +0200

    Fixed mistakes in Rust implementation of edit distance.

--- Rust/edit_distance/src/main.rs
+++ Rust/edit_distance/src/main.rs
@@ -3,18 +3,26 @@
 // * https://doc.rust-lang.org/std/primitive.str.html
 // * https://doc.rust-lang.org/std/index.html
 
+// Indexing vector of vectors:
+//  vec[i][j]
+//    v = vec![vec![0usize; len1]; len2];
+//    v is a vector of length len2 that contains vectors of length len1
+//    v[i] -- returns i-th vector, i \in [0, len2)
+//    v[i][j] -- returns j-th item in i-th vector, j \in [0, len1)
+
 fn edit_distance(s1: &str, s2: &str) -> usize {
     let len1 = s1.chars().count();
     let len2 = s2.chars().count();
 
+    // Table with all items isn't necessary
     let mut tab = vec![vec![0usize; len1]; len2];
 
-    for j in 1..len2 {
+    for j in 1..len1 {
         tab[0][j] = j;
     }
-    for (i, c2) in s2.chars().skip(1).enumerate() {
+    for (i, c2) in s2.chars().enumerate().skip(1) {
         tab[i][0] = i;
-        for (j, c1) in s1.chars().skip(1).enumerate() {
+        for (j, c1) in s1.chars().enumerate().skip(1) {
             tab[i][j] = if c1 == c2 {
                 tab[i - 1][j - 1]
             } else {
@@ -25,7 +33,7 @@ fn edit_distance(s1: &str, s2: &str) -> usize {
         }
     }
 
-    tab[len1 - 1][len2 - 1]
+    tab[len2 - 1][len1 - 1]
 }
 
 fn main() {
