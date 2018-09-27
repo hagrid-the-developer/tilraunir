e0bfeeacf79a2d1f6d0618aea88085866a7ac81a
Daniel Fiala <freelancer@meave.biz>
2018-09-27 06:09:22 +0200

    Edit distance in Rust: Switched to array with two rows.

--- Rust/edit_distance/src/main.rs
+++ Rust/edit_distance/src/main.rs
@@ -10,56 +10,81 @@
 //    v[i] -- returns i-th vector, i \in [0, len2)
 //    v[i][j] -- returns j-th item in i-th vector, j \in [0, len1)
 
+// I wanted to do something like this:
+//   let prev = &tab[(i - 1) % 2];
+//   let act = &mut tab[i % 2];
+//
+// But it seems that Rust doesn't allow to access
+//   one object with two references (where one is mutable),
+//   even if the references access different parts of the object.
+
 fn edit_distance(s1: &str, s2: &str) -> usize {
     let len1 = s1.chars().count();
     let len2 = s2.chars().count();
 
     // Table with all items isn't necessary
-    let mut tab = vec![vec![0usize; len1]; len2];
+    let mut tab = vec![vec![0usize; len1]; 2];
 
     for j in 1..len1 {
         tab[0][j] = j;
     }
     for (i, c2) in s2.chars().enumerate().skip(1) {
-        tab[i][0] = i;
+        let idx_prev = (i - 1) % 2;
+        let idx_act = i % 2;
+
+        tab[idx_act][0] = i;
+
         for (j, c1) in s1.chars().enumerate().skip(1) {
-            tab[i][j] = if c1 == c2 {
-                tab[i - 1][j - 1]
-            } else {
-                let x = std::cmp::min(tab[i - 1][j], tab[i][j - 1]);
-                let y = tab[i - 1][j - 1];
-                1 + std::cmp::min(x, y)
+            tab[idx_act][j] = {
+                let prev = &tab[idx_prev][..];
+                let act = &tab[idx_act][..];
+
+                if c1 == c2 {
+                    prev[j - 1]
+                } else {
+                    let x = std::cmp::min(prev[j], act[j - 1]);
+                    let y = prev[j - 1];
+                    1 + std::cmp::min(x, y)
+                }
             }
         }
     }
 
-    tab[len2 - 1][len1 - 1]
+    tab[(len2 - 1) % 2][len1 - 1]
 }
 
 fn edit_distance_zip(s1: &str, s2: &str) -> usize {
     let len1 = s1.chars().count();
     let len2 = s2.chars().count();
 
-    // Table with all items isn't necessary
-    let mut tab = vec![vec![0usize; len1]; len2];
+    let mut tab = vec![vec![0usize; len1]; 2];
 
     for j in 1..len1 {
         tab[0][j] = j;
     }
     for (c2, i) in s2.chars().skip(1).zip(1usize..) {
-        tab[i][0] = i;
+        let idx_prev = (i - 1) % 2;
+        let idx_act = i % 2;
+
+        tab[idx_act][0] = i;
+
         for (c1, j) in s1.chars().skip(1).zip(1usize..) {
-            tab[i][j] = if c1 == c2 {
-                tab[i - 1][j - 1]
-            } else {
-                let x = std::cmp::min(tab[i - 1][j], tab[i][j - 1]);
-                let y = tab[i - 1][j - 1];
-                1 + std::cmp::min(x, y)
+            tab[idx_act][j] = {
+                let prev = &tab[idx_prev][..];
+                let act = &tab[idx_act][..];
+
+                if c1 == c2 {
+                    prev[j - 1]
+                } else {
+                    let x = std::cmp::min(prev[j], act[j - 1]);
+                    let y = prev[j - 1];
+                    1 + std::cmp::min(x, y)
+                }
             }
         }
     }
 
-    tab[len2 - 1][len1 - 1]
+    tab[(len2 - 1) % 2][len1 - 1]
 }
 
 fn main() {
