d61bf603ac91c920593665b707af23899759e112
Daniel Fiala <freelancer@meave.biz>
2020-02-28 14:55:34 +0100

    ffmpeg: Compilation under mac & extracting source info.

---
+++ ffmpeg/xyz/extract-info.c
@@ -0,0 +1,61 @@
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+
+#include <stdio.h>
+
+// compatibility with newer API
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(55,28,1)
+#define av_frame_alloc avcodec_alloc_frame
+#define av_frame_free avcodec_free_frame
+#endif
+
+int main(int argc, char *argv[]) {
+  // Initalizing these to NULL prevents segfaults!
+  AVFormatContext   *pFormatCtx = NULL;
+  int               i, videoStream;
+  AVCodecContext    *pCodecCtxOrig = NULL;
+  AVCodecContext    *pCodecCtx = NULL;
+  AVCodec           *pCodec = NULL;
+  AVFrame           *pFrame = NULL;
+  AVFrame           *pFrameRGB = NULL;
+  AVPacket          packet;
+  int               frameFinished;
+  int               numBytes;
+  uint8_t           *buffer = NULL;
+  struct SwsContext *sws_ctx = NULL;
+
+  if(argc < 2) {
+    printf("Please provide a movie file\n");
+    return -1;
+  }
+  // Register all formats and codecs
+  //av_register_all(); // <-- deprecated
+
+  // Open video file
+  if(avformat_open_input(&pFormatCtx, argv[1], NULL, NULL)!=0)
+    return -1; // Couldn't open file
+
+  // Retrieve stream information
+  if(avformat_find_stream_info(pFormatCtx, NULL)<0)
+    return -1; // Couldn't find stream information
+
+  // Dump information about file onto standard error
+  av_dump_format(pFormatCtx, 0, argv[1], 0);
+
+  // Free the RGB image
+  av_free(buffer);
+  av_frame_free(&pFrameRGB);
+
+  // Free the YUV frame
+  av_frame_free(&pFrame);
+
+  // Close the codecs
+  avcodec_close(pCodecCtx);
+  avcodec_close(pCodecCtxOrig);
+
+  // Close the video file
+  avformat_close_input(&pFormatCtx);
+
+  return 0;
+}
